e6b161314863eb58a3e7655f91dd135d7b78f72d
indiamogambo3 <132776421+indiamogambo3@users.noreply.github.com>
2023-05-08 20:21:05 +0100

    Update Programme01.java

--- src/java_homework_week01/Programme01.java
+++ src/java_homework_week01/Programme01.java
@@ -4,13 +4,13 @@ public class Programme01 {
 
     public static void main(String [] args) {
 
-        System.out.println("Name:             Mukesh Shah"                  );
-        System.out.println("Date of Birth:    15/05/1978"                   );
+        System.out.println("Name:             Michael Smith"                  );
+        System.out.println("Date of Birth:    12/10/1976"                   );
         System.out.println("Gender:           Male"                         );
         System.out.println("Mobile No:        07452 258 369"                );
         System.out.println("Nationality:      British      "                );
-        System.out.println("Email:            mogambo@gmail.com"            );
-        System.out.println("Religion:         Hindu"                        );
+        System.out.println("Email:            m.smith@gmail.com"            );
+        System.out.println("Religion:         Catholic"                        );
         System.out.println("Hobbies:          Football, Badminton"          );
         System.out.println("Address:          142 Palace Street, London, W1");
 
